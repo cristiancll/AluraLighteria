30d243d046dd982eb5ecf6a8aedbe7166ab19438
cristiancll <criscllorente@gmail.com>
2021-07-11 19:25:33 -0300

    Module 4, Section 4 - implementation of background component

--- src/views/componentes/DetalhesProduto/componentes/background/index.js
+++ src/views/componentes/DetalhesProduto/componentes/background/index.js
@@ -1,8 +1,56 @@
 import React from "react";
-import {View} from 'react-native';
+import {Dimensions, Image, ImageBackground, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
+import {Sacola} from "../../../../../componentes/sacola";
 
 export const Background = () => {
+  const imgSrc = require('../../../../../assets/images/bgimg.jpg');
+  const navigation = useNavigation();
+
   return (
-      <View/>
+      <View style={styles.bgContainer}>
+          <ImageBackground
+              source={imgSrc}
+              resizeMode="cover"
+              style={styles.imageBackground}
+          >
+
+              <View style={styles.headerContainer}>
+                  <TouchableOpacity onpress={() => navigation.push('App')}>
+                      <Image
+                          source={require('../../../../../assets/images/flecha-esquerda.png')}
+                          style={styles.returnImage}
+                      />
+                  </TouchableOpacity>
+                  <View style={styles.shoppingBagContainer}>
+                      <Sacola/>
+                  </View>
+              </View>
+          </ImageBackground>
+      </View>
   );
 };
+
+const styles = StyleSheet.create({
+  bgContainer: {
+    flex: 1.5,
+  },
+  imageBackground: {
+      width: Dimensions.get('window').width,
+      height: '100%'
+  },
+  headerContainer: {
+      flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  returnImage: {
+    width: 24,
+    height: 24,
+    marginTop: 36,
+    marginLeft: 24,
+  },
+  shoppingBagContainer: {
+    padding: 18,
+  }
+});
+
